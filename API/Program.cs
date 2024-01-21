caa3156c033cb9926799a02887be50135321a0ce
andresrodpaz <ae.rodpaz@gmail.com>
2024-01-21 17:51:41 +0100

    adding ssl certifcate

--- API/Program.cs
+++ API/Program.cs
@@ -12,23 +12,18 @@ builder.Services.AddDbContext<DataContext>(opt => {
     // Configuring the DbContext to use SQLite as the database provider.
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+
